bbb8f6c317ae18a903e39cf664f9045025c99612
utsavpatel22 <erutsavpatel22@gmail.com>
2020-05-19 01:57:58 -0400

    Finished linearizing pose2pose and pose2landmark constraints

--- lsslam_framework/octave/linearize_pose_landmark_constraint.m
+++ lsslam_framework/octave/linearize_pose_landmark_constraint.m
@@ -11,6 +11,16 @@
 function [e, A, B] = linearize_pose_landmark_constraint(x, l, z)
 
   % TODO compute the error and the Jacobians of the error
+  X = v2t(x);
+  e = X(1:2,1:2)' * (l - x(1:2)) - z;
+  A = zeros(2,3);
+  A(:,1) = [-cos(x(3)); sin(x(3))];
+  A(:,2) = [-sin(x(3)); -cos(x(3))];
+  A(:,3) = [-(l(1) - x(1)) * sin(x(3)) + (l(2) - x(2)) * cos(x(3)); -(l(1) - x(1)) * cos(x(3)) - (l(2) - x(2)) * sin(x(3))];
+  
+  B = zeros(2,2);
+  B(:,1) = [cos(x(3)); -sin(x(3))];
+  B(:,2) = [sin(x(3)); cos(x(3))];
 
 
 end;
--- lsslam_framework/octave/linearize_pose_pose_constraint.m
+++ lsslam_framework/octave/linearize_pose_pose_constraint.m
@@ -14,6 +14,20 @@
 function [e, A, B] = linearize_pose_pose_constraint(x1, x2, z)
 
   % TODO compute the error and the Jacobians of the error
+  X1 = v2t(x1);
+  X2 = v2t(x2);
+  Z = v2t(z);
+  
+  e = [[Z(1:2,1:2)' * (X1(1:2,1:2)' * (x2(1:2) - x1(1:2)) - z(1:2))]; x2(3) - x1(3) - z(3)];
+  A = zeros(3,3);
+  A(:,1) = [ (-cos(x1(3)) * cos(z(3))) + sin(x1(3)) * sin(z(3)); cos(x1(3)) * sin(z(3)) + sin(x1(3)) * cos(z(3)); 0];
+  A(:,2) = [ (-sin(x1(3)) * cos(z(3))) - (cos(x1(3)) * sin(z(3))); sin(x1(3)) * sin(z(3)) - (cos(x1(3)) * cos(z(3))); 0];
+  A(:,3) = [ (x2(1) - x1(1)) * (-sin(x1(3)) * cos(z(3))) + (x2(2) - x1(2)) * cos(x1(3)) * cos(z(3)) - (x2(1) - x1(1)) * cos(x1(3)) * sin(z(3)) - (x2(2) - x1(2)) * sin(x1(3)) * sin(z(3)) ; (x2(1) - x1(1)) * sin(x1(3)) * sin(z(3)) - (x2(2) - x1(2)) * cos(x1(3)) * sin(z(3)) - (x2(1) - x1(1)) * cos(x1(3)) * cos(z(3)) - (x2(2) - x1(2)) * sin(x1(3)) * cos(z(3)); -1];
+  
+  B = zeros(3,3);
+  B(:,1) = [cos(x1(3)) * cos(z(3)) - sin(x1(3)) * sin(z(3)); -cos(x1(3)) * sin(z(3)) - sin(x1(3)) * cos(z(3)); 0];
+  B(:,2) = [sin(x1(3)) * cos(z(3)) + cos(x1(3)) * sin(z(3)); -sin(x1(3)) * sin(z(3)) + cos(x1(3)) * cos(z(3)); 0];
+  B(:,3) = [0;0;1];
 
 
 end;
